4f6410f060dbe3e47f1297a708754502bf1fe094
shivasapra <ashishsapra7@gmail.com>
2019-07-08 12:02:30 +0530

    Client Vendor And Employee Controller Setup With Views

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,28 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/vendors', 'VendorController@index')->name('vendors');
+Route::get('/vendor/create', 'VendorController@create')->name('vendor.create');
+Route::post('/vendor/store', 'VendorController@store')->name('vendor.store');
+Route::get('/vendor/edit/{id}', 'VendorController@edit')->name('vendor.edit');
+Route::post('/vendor/update/{id}', 'VendorController@update')->name('vendor.update');
+Route::get('/vendor/view/{id}', 'VendorController@show')->name('vendor.view');
+Route::get('/vendor/delete/{id}', 'VendorController@destroy')->name('vendor.delete');
+
+
+Route::get('/clients', 'ClientController@index')->name('clients');
+Route::get('/client/create', 'ClientController@create')->name('client.create');
+Route::post('/client/store', 'ClientController@store')->name('client.store');
+Route::get('/client/edit/{id}', 'ClientController@edit')->name('client.edit');
+Route::post('/client/update/{id}', 'ClientController@update')->name('client.update');
+Route::get('/client/view/{id}', 'ClientController@show')->name('client.view');
+Route::get('/client/delete/{id}', 'ClientController@destroy')->name('client.delete');
+
+Route::get('/employees', 'EmployeeController@index')->name('employees');
+Route::get('/employee/create', 'EmployeeController@create')->name('employee.create');
+Route::post('/employee/store', 'EmployeeController@store')->name('employee.store');
+Route::get('/employee/edit/{id}', 'EmployeeController@edit')->name('employee.edit');
+Route::post('/employee/update/{id}', 'EmployeeController@update')->name('employee.update');
+Route::get('/employee/view/{id}', 'EmployeeController@show')->name('employee.view');
+Route::get('/employee/delete/{id}', 'EmployeeController@destroy')->name('employee.delete');
